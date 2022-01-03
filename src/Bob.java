e99d2155816b10e497fcaa21e4b901faf9a5b865
gary_s <smithgd34@gmail.com>
2022-01-03 15:00:46 -0600

    Bob.

--- src/Bob.java
+++ src/Bob.java
@@ -6,31 +6,31 @@ public class Bob {
     public static void main(String[] args) {
         String stillChatting = "";
         do {
-        System.out.println("Say what you would like to Bob, be sure to use punctuation!?!");
+        System.out.println("Say what you would like to Bob, be sure to use punctuation!?! ");
         String userInput = sc.nextLine();
 
-        switch (userInput) {
-            case "":
-                System.out.println("Fine. Be that way!");
-                break;
-            case "?":
-                System.out.println("Sure");
-                break;
-            case "!":
-                System.out.println("Whoa, chill out!");
-                break;
-            default:
-                System.out.println("Whatever.");
-        }
-//            if (userInput.equals("")) {
+//        switch (userInput) {
+//            case "":
 //                System.out.println("Fine. Be that way!");
-//            } else if (userInput.endsWith("?")) {
+//                break;
+//            case "?":
 //                System.out.println("Sure");
-//            } else if (userInput.endsWith("!")) {
+//                break;
+//            case "!":
 //                System.out.println("Whoa, chill out!");
-//            } else {
+//                break;
+//            default:
 //                System.out.println("Whatever.");
-//            }
+//        }
+            if (userInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else if (userInput.endsWith("?")) {
+                System.out.println("Sure");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else {
+                System.out.println("Whatever.");
+            }
             System.out.println("You gonna take that from him?!?! Y/N");
             stillChatting = sc.nextLine();
         } while (stillChatting.equalsIgnoreCase("n"));

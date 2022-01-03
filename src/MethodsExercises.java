509223aa07bfa798aa1237e05d72334038c56058
gary_s <smithgd34@gmail.com>
2022-01-03 14:30:17 -0600

    Complete up to number 3.

---
+++ src/MethodsExercises.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class MethodsExercises {
+//    public static void add(int num1, int num2) {
+//        System.out.println(num1 + num2);
+//    }
+//     public static void subtract(int num1, int num2) {
+//        System.out.println(num1 - num2);
+//    }
+//     public static void multiply(int num1, int num2) {
+//        System.out.println(num1 * num2);
+//    }
+//
+//    public static void noAstrick(int num1, int num2) {
+//        int result = 0;
+//        for (int i = 1; i <= num1; i++) {
+//            result += num2;
+//        }
+//            System.out.println(result);
+//    }
+//     public static int recMulti(int num1, int num2) {
+//        if (num1 == 0 || num2 == 0) {
+//            return 0;
+//        } else {
+//            int result = (num1 + recMulti(num1, num2 -1));
+//            return result;
+//        }
+//
+//    }
+//
+//     public static void divide(int num1, int num2) {
+//        System.out.println(num1 / num2);
+//    }
+//      public static void modulus(int num1, int num2) {
+//        System.out.println(num1 % num2);
+//    }
+
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        if (userInput <= max && userInput >= min) {
+            return userInput;
+        }
+        System.out.println("Invalid input try again: ");
+        return getInteger(1, 10);
+
+    }
+
+    public static void main(String[] args) {
+//        add(5, 5);
+//        subtract(10, 5);
+//        multiply(5, 5);
+//        noAstrick(5, 5);
+//        System.out.println(recMulti(5, 5));
+//        divide(10, 5);
+//        divide(10, 0);
+//        modulus(11, 5);
+
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
+        System.out.println("Your number is within range!");
+
+    }
+}

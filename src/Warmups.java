88537602130b14980a1c962b8651ec9e986f1490
gary_s <smithgd34@gmail.com>
2022-01-04 09:05:26 -0600

    Warmup complete.

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 public class Warmups {
+
+    public static String startDay() {
+        return "Brain loading... Done!";
+    }
+
+
     public static void main(String[] args) {
 //        System.out.println("Java is statically type, while Javascript is dynamically typed.");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What did yo ueat for breakfast today?");
+        System.out.println("What did you eat for breakfast today?");
         String userBreakfast = scanner.next();
         System.out.printf("I don't eat breakfast but %s sound(s) really good!", userBreakfast);
+        System.out.println();
+        System.out.println(startDay());
 
     }
 }
+

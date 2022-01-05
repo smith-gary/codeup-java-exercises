a6935d3bb75f5f326418f4a974d7d7a7e82052ca
gary_s <smithgd34@gmail.com>
2022-01-05 11:41:47 -0600

    Completed exercises without bonuses.

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,14 @@
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input sc = new Input();
+        System.out.println("What is the radius of the circle? ");
+        double userInput = sc.getScanner().nextDouble();
+        Circle rad = new Circle(userInput);
+        System.out.println(rad.getArea());
+        System.out.println(rad.getCircumference());
+    }
+}

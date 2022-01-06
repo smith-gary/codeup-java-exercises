fdaa2952e144203c0d09bddb77a3b5898b7e5703
gary_s <smithgd34@gmail.com>
2022-01-06 09:32:10 -0600

    Completed warmup for OOP.

---
+++ src/Penguin.java
@@ -0,0 +1,55 @@
+public class Penguin {
+
+    private String name;
+    private int weight;
+    private String species;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public Penguin() {
+
+    }
+
+    public static boolean canFly() {
+        return false;
+    }
+
+    public static boolean areEvil() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
